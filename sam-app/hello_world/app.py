0fd9ea70d7af3404677d4adc8e04f48ac5f61940
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-08-03 15:31:33 +0530

    Add files via upload

---
+++ sam-app/hello_world/app.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    first_name = event['first_name']
+    last_name = event['last_name']  
+    message = event['message']  
+
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": f"{message} {first_name} {last_name}",
+        }),
+    }
